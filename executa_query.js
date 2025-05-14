a216bf5bf9696a48bd98bb6e4438b7d99583707f
Mariavss <127995230+Mariavss@users.noreply.github.com>
2025-05-14 11:26:21 -0400

    Update executa_query.js

--- executa_query.js
+++ executa_query.js
@@ -5,9 +5,9 @@ http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     var q = url.parse(req.url, true).query;
-    var txt = (q.ano && q.mes) ? q.ano + " " + q.mes : "";
+    var txt = q.ano + " " + q.mes;
     res.write(txt)
-    res.write('<p><a href="/?ano=2023&mes=Julho">Teste</a></p>');
+    res.write('<p><a href="/?ano=2023&mes=Julho">Test</a></p>');
     res.end();
   })
   .listen(5000);
